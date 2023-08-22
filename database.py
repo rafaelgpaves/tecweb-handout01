dc8c839df53959fd822aece062074dd4170f4aa6
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2023-08-22 08:21:21 -0300

    handout 3 parte 2

---
+++ database.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+class Database():
+
+    def __init__(self, banco) -> None:
+        self.conn = sqlite3.connect(banco + ".db")
+
+        self.cursor = self.conn.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS note ( id INTEGER PRIMARY KEY, title TEXT, content TEXT NOT NULL)")
