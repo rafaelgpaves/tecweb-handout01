1fbaf99bc6ccd7245dc56fe9284f8a9d4ead51e1
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2023-08-22 08:45:30 -0300

    handout 3 parte 3

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from dataclasses import dataclass
 
 class Database():
 
@@ -7,3 +8,13 @@ class Database():
 
         self.cursor = self.conn.cursor()
         self.cursor.execute("CREATE TABLE IF NOT EXISTS note ( id INTEGER PRIMARY KEY, title TEXT, content TEXT NOT NULL);")
+
+    def add(self, note):
+        self.cursor.execute(f"INSERT INTO note (title,content) VALUES ('{note.title}','{note.content}');")
+        self.conn.commit()
+
+@dataclass
+class Note:
+    id: int = None
+    title: str = None
+    content: str = ''
