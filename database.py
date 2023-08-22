c85a6511fee6d69afeda5bb5ed48e11b8eaaa066
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2023-08-22 08:27:47 -0300

    handount 3 parte 2 completa

--- database.py
+++ database.py
@@ -6,4 +6,4 @@ class Database():
         self.conn = sqlite3.connect(banco + ".db")
 
         self.cursor = self.conn.cursor()
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS note ( id INTEGER PRIMARY KEY, title TEXT, content TEXT NOT NULL)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS note ( id INTEGER PRIMARY KEY, title TEXT, content TEXT NOT NULL);")
